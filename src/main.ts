52c381d446c8be284c8e56bd0847ecbc85e78192
Igor Kotenko <igor.kotenko.1995@yandex.ru>
2025-12-02 12:15:03 +0300

    Проект поставлен на vite

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import './assets/styles.scss';
+
+const app = createApp(App);
+app.use(createPinia());
+app.mount('#app');
