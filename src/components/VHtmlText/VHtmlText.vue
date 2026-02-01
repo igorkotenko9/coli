1dbce7f35e315fb0b278a43ea103cd85aec08024
Igor Kotenko <igor.kotenko.1995@yandex.ru>
2026-02-01 16:30:49 +0300

    Добавлен компонент VHtmlText

---
+++ src/components/VHtmlText/VHtmlText.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-html="text" class="v-html-text" />
+</template>
+
+<script lang="ts">
+import { type ExtractPublicPropTypes } from "vue";
+
+import { propsFactory } from "@utils";
+
+export const getProps = propsFactory({
+  text: {
+    type: String,
+    required: true,
+  },
+});
+export type Props = ExtractPublicPropTypes<ReturnType<typeof getProps>>;
+</script>
+
+<script setup lang="ts">
+defineProps(getProps());
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/components/VHtmlText/index.ts
@@ -0,0 +1,5 @@
+export {
+  type Props as VHtmlTextProps,
+  default,
+  getProps as getVHtmlTextProps,
+} from "./VHtmlText.vue";
