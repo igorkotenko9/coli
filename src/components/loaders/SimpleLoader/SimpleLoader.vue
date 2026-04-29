01550882b43931c202a99d1a1407c94bc2890e69
igorkotenko9 <igor.kotenko.1995@yandex.ru>
2026-04-29 16:25:57 +0300

    добавлен компонент SimpleLoader

---
+++ src/components/loaders/SimpleLoader/SimpleLoader.vue
@@ -0,0 +1,155 @@
+<template>
+  <div class="simple-loader">
+    <div
+      class="simple-loader__inner"
+      :class="{
+        'simple-loader__inner--two-lined-round':
+          variation === 'two-lined-round',
+      }"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { type PropType } from "vue";
+
+defineProps({
+  variation: String as PropType<"two-lined-round">,
+});
+</script>
+
+<style scoped lang="scss">
+@keyframes l20-1 {
+  0% {
+    clip-path: polygon(50% 50%, 0 0, 50% 0%, 50% 0%, 50% 0%, 50% 0%, 50% 0%);
+  }
+
+  12.5% {
+    clip-path: polygon(
+      50% 50%,
+      0 0,
+      50% 0%,
+      100% 0%,
+      100% 0%,
+      100% 0%,
+      100% 0%
+    );
+  }
+
+  25% {
+    clip-path: polygon(
+      50% 50%,
+      0 0,
+      50% 0%,
+      100% 0%,
+      100% 100%,
+      100% 100%,
+      100% 100%
+    );
+  }
+
+  50% {
+    clip-path: polygon(
+      50% 50%,
+      0 0,
+      50% 0%,
+      100% 0%,
+      100% 100%,
+      50% 100%,
+      0% 100%
+    );
+  }
+
+  62.5% {
+    clip-path: polygon(
+      50% 50%,
+      100% 0,
+      100% 0%,
+      100% 0%,
+      100% 100%,
+      50% 100%,
+      0% 100%
+    );
+  }
+
+  75% {
+    clip-path: polygon(
+      50% 50%,
+      100% 100%,
+      100% 100%,
+      100% 100%,
+      100% 100%,
+      50% 100%,
+      0% 100%
+    );
+  }
+
+  100% {
+    clip-path: polygon(
+      50% 50%,
+      50% 100%,
+      50% 100%,
+      50% 100%,
+      50% 100%,
+      50% 100%,
+      0% 100%
+    );
+  }
+}
+
+@keyframes l20-2 {
+  0% {
+    transform: scaleY(1) rotate(0deg);
+  }
+
+  49.99% {
+    transform: scaleY(1) rotate(135deg);
+  }
+
+  50% {
+    transform: scaleY(-1) rotate(0deg);
+  }
+
+  100% {
+    transform: scaleY(-1) rotate(-135deg);
+  }
+}
+
+@keyframes l1 {
+  0% {
+    transform: rotate(0.45turn);
+  }
+
+  50% {
+    transform: rotate(3.95turn);
+  }
+
+  100% {
+    transform: rotate(8.45turn);
+  }
+}
+
+.simple-loader {
+  width: var(--simple-loader-width);
+  height: var(--simple-loader-height);
+  aspect-ratio: 1;
+
+  &__inner {
+    width: inherit;
+    height: inherit;
+    border: 2px solid currentcolor;
+
+    border-radius: 50%;
+
+    animation:
+      l20-1 0.8s infinite linear alternate,
+      l20-2 1.6s infinite linear;
+
+    &--two-lined-round {
+      border-color: currentcolor transparent;
+
+      animation: l1 3s infinite;
+    }
+  }
+}
+</style>
