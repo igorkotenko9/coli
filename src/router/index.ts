01268610530ea3ce9ede3b944e94a52950ab4e8a
Igor Kotenko <igor.kotenko.1995@yandex.ru>
2026-09-24 23:38:48 +0300

    Switch to history mode with _redirects for clean URLs

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import {
   useRouter as _useRouter,
   createRouter,
-  createWebHashHistory,
+  createWebHistory,
 } from "vue-router";
 
 const routes = [
@@ -44,7 +44,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(import.meta.env.VITE_BASE_URL),
   routes: routes,
 });
 
